58f2b5b53506392807f678f5d5c0c1829668002b
Kevin Ngo <ngoke@mozilla.com>
2014-11-25 04:10:54 -0800

    fix page title

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Marketplace'
+project = u'Marketplace Frontend'
 copyright = u'2014, Marketplace Developers'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -215,7 +215,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'marketplace', u'Marketplace Documentation',
+    ('index', 'marketplace', u'Marketplace Frontend Documentation',
      [u'Marketplace Developers'], 1)
 ]
 
@@ -229,7 +229,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'Marketplace', u'Marketplace Documentation',
+  ('index', 'Marketplace', u'Marketplace Frontend Documentation',
    u'Marketplace Developers', 'Marketplace', 'One line description of project.',
    'Miscellaneous'),
 ]
